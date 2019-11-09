9e86c76784db046c973914839128da1d2a8ff007
Your Name <you@example.com>
2019-11-09 09:54:54 -0500

    Updates

---
+++ precache-manifest.595e624eeb9766e23417e7392a9f381e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7e9faaf06dad5027b163c996c8b4d3",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "1a30130e66c057ebfc12",
+    "url": "/Clicky-Game/static/css/main.5f988438.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/Clicky-Game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "1a30130e66c057ebfc12",
+    "url": "/Clicky-Game/static/js/main.4370f7b5.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
